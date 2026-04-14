de86a7606c714454cc4a8f39cb79a9f13d87b5cd
forant <tforan@gmail.com>
2026-04-14 14:42:47 -0700

    Initial Commit

---
+++ SmarterTraining/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SmarterTraining
+//
+//  Created by Tim Foran on 4/14/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
